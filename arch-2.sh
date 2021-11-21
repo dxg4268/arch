289b9acd1eb1fccfb00dc1a414165033c69d85f3
Aditya Sharma <65886209+dxg4268@users.noreply.github.com>
2021-11-21 18:39:48 +0530

    Update arch-2.sh

--- arch-2.sh
+++ arch-2.sh
@@ -16,13 +16,14 @@ echo "::1		      localhost" >> /etc/hosts
 echo "127.0.1.1		arch.localdomain	arch" >> /etc/hosts
 
 #Bootloader
-pacman -S grub grub-btrfs os-prober ntfs-3g
+pacman -S grub grub-btrfs os-prober ntfs-3g --noconfirm
 os-prober
 grub-install /dev/sda
+echo "GRUB_DISABLE_OS_PROBER=true" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #Extra packages
-pacman -S vlc xorg base-devel git go gnome firefox
+pacman -S vlc xorg base-devel git go xfce4 xfce4-goodies sddm firefox --noconfirm
 
 #Chaotic AUR and imp AUR Packages
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
@@ -32,10 +33,10 @@ echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 echo "[chaotic-aur]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-pacman -Sy pamac-aur archlinux-appstream-data libpamac-aur yay
+pacman -Sy pamac-aur archlinux-appstream-data libpamac-aur yay --noconfirm
 
 #enable service
 systemctl enable ufw
 systemctl enable NetworkManager
-systemctl enable gdm
+systemctl enable sddm
 
