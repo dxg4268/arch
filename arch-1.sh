ac9a6c936f33fea8bb36a6a01c19f56f6a54de16
Aditya Sharma <65886209+dxg4268@users.noreply.github.com>
2021-11-28 17:48:06 +0530

    Update arch-1.sh

--- arch-1.sh
+++ arch-1.sh
@@ -1,5 +1,5 @@
 #Refresh mirrors
-#reflector --verbose -c "India" --sort rate > /etc/pacman.d/mirrorlist
+reflector --verbose -c "India" --sort rate > /etc/pacman.d/mirrorlist
 pacman -Syy > /dev/null 2>&1
 echo
 echo
@@ -9,19 +9,19 @@ echo
 
 
 #Disks
-mkfs.btrfs /dev/sda3 -f > /dev/null 2>&1
-mkswap /dev/sda4 > /dev/null 2>&1
-swapon /dev/sda4 > /dev/null 2>&1
-mount /dev/sda3 /mnt
+mkfs.btrfs /dev/sdb3 -f > /dev/null 2>&1
+mkswap /dev/sdb4 > /dev/null 2>&1
+swapon /dev/sdb4 > /dev/null 2>&1
+mount /dev/sdb3 /mnt
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
 btrfs su cr /mnt/@var
 umount /mnt
-mount -o noatime,compress=zstd,discard=async,space_cache,subvol=@ /dev/sda3 /mnt
+mount -o noatime,compress=zstd,discard=async,subvol=@ /dev/sdb3 /mnt
 mkdir /mnt/home
 mkdir /mnt/var
-mount -o noatime,compress=zstd,discard=async,space_cache,subvol=@home /dev/sda3 /mnt/home
-mount -o noatime,compress=zstd,discard=async,space_cache,subvol=@var /dev/sda3 /mnt/var
+mount -o noatime,compress=zstd,discard=async,subvol=@home /dev/sdb3 /mnt/home
+mount -o noatime,compress=zstd,discard=async,subvol=@var /dev/sdb3 /mnt/var
 
 echo 
 echo "--------------------------------------------------------"
@@ -33,13 +33,13 @@ echo
 echo "--------------------------------------------------------"
 echo
 
-sleep 15s
+sleep 10s
 
 #Install Base System
 echo
 echo
 echo "[+] Installing Packages to the new root, this might take some time."
-pacstrap /mnt base linux-zen linux-firmware sudo nano vi vim networkmanager ufw > /dev/null 2>&1
+pacstrap /mnt base linux-zen linux-firmware sudo nano vi vim networkmanager intel-ucode ufw > /dev/null 2>&1
 echo
 echo
 
