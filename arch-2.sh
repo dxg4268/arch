5d73a5713dc6dc90061242e65ffb21a293d42b94
Aditya Sharma <65886209+dxg4268@users.noreply.github.com>
2021-11-28 13:19:46 +0530

    Update arch-2.sh

--- arch-2.sh
+++ arch-2.sh
@@ -1,5 +1,5 @@
 #Time
-pacman -S ntp
+pacman -S ntp --noconfirm > /dev/null 2>&1
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 timedatectl set-ntp true
 hwclock --systohc
@@ -16,24 +16,24 @@ echo "::1		      localhost" >> /etc/hosts
 echo "127.0.1.1		arch.localdomain	arch" >> /etc/hosts
 
 #Bootloader
-pacman -S grub grub-btrfs os-prober ntfs-3g --noconfirm
+pacman -S grub grub-btrfs os-prober ntfs-3g --noconfirm > /dev/null 2>&1
 os-prober
 grub-install /dev/sda
-echo "GRUB_DISABLE_OS_PROBER=true" >> /etc/default/grub
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #Extra packages
-pacman -S vlc xorg base-devel git go xfce4 xfce4-goodies sddm firefox --noconfirm
+pacman -S vlc xorg base-devel git go openbox picom sddm firefox ttf-liberation ttf-dejavu --noconfirm
 
 #Chaotic AUR and imp AUR Packages
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-'{keyring,mirrorlist}'.pkg.tar.zst'
+#pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+#pacman-key --lsign-key 3056513887B78AEB
+#pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-'{keyring,mirrorlist}'.pkg.tar.zst'
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
-echo "[chaotic-aur]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-pacman -Sy pamac-aur archlinux-appstream-data libpamac-aur yay --noconfirm
+#echo "[chaotic-aur]" >> /etc/pacman.conf
+#echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+#pacman -Sy pamac-aur archlinux-appstream-data libpamac-aur yay --noconfirm
 
 #enable service
 systemctl enable ufw
