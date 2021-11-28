563248a5fdde40a46ba2a1f00d79748adb4da931
Aditya Sharma <65886209+dxg4268@users.noreply.github.com>
2021-11-28 13:05:47 +0530

    Update arch-1.sh

--- arch-1.sh
+++ arch-1.sh
@@ -1,21 +1,48 @@
 #Refresh mirrors
-reflector --verbose -c "India" --sort rate > /etc/pacman.d/mirrorlist
-pacman -Syy
+#reflector --verbose -c "India" --sort rate > /etc/pacman.d/mirrorlist
+pacman -Syy > /dev/null 2>&1
+echo
+echo
+echo "[+] All Package Databases were updated !"
+echo
+echo
+
 
 #Disks
-mkfs.btrfs /dev/sda3 -f
-mkswap /dev/sda4
-swapon /dev/sda4
+mkfs.btrfs /dev/sda3 -f > /dev/null 2>&1
+mkswap /dev/sda4 > /dev/null 2>&1
+swapon /dev/sda4 > /dev/null 2>&1
 mount /dev/sda3 /mnt
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
+btrfs su cr /mnt/@var
 umount /mnt
-mount -o noatime,compress=zstd,subvol=@ /dev/sda3 /mnt
+mount -o noatime,compress=zstd,discard=async,space_cache,subvol=@ /dev/sda3 /mnt
 mkdir /mnt/home
-mount -o noatime,compress=zstd,subvol=@home /dev/sda3 /mnt/home
+mkdir /mnt/var
+mount -o noatime,compress=zstd,discard=async,space_cache,subvol=@home /dev/sda3 /mnt/home
+mount -o noatime,compress=zstd,discard=async,space_cache,subvol=@var /dev/sda3 /mnt/var
+
+echo 
+echo "--------------------------------------------------------"
+echo
+
+lsblk
+
+echo
+echo "--------------------------------------------------------"
+echo
+
+sleep 15s
 
 #Install Base System
-pacstrap /mnt base linux-zen linux-firmware sudo nano vi vim networkmanager ufw
+echo
+echo
+echo "[+] Installing Packages to the new root, this might take some time."
+pacstrap /mnt base linux-zen linux-firmware sudo nano vi vim networkmanager ufw > /dev/null 2>&1
+echo
+echo
+
 
 #Generate fstab
 genfstab -U -p /mnt >> /mnt/etc/fstab
